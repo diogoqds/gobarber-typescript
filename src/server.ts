b59794ea3ab48433ffb1ef8d133fe93d62f73030
Diogo Queiroz <diogo.qds97@gmail.com>
2020-05-01 17:51:07 -0300

    first commit

---
+++ src/server.ts
@@ -0,0 +1,8 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => res.json({ hello: 'world' }));
+app.listen(3333, () => {
+  console.log('Server running on port 3333! 🚀');
+});
