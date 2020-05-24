b48d87d00fb7ad50377be6b982732f0756212070
Diogo Queiroz <diogo.qds97@gmail.com>
2020-05-24 15:22:51 -0300

    generate jwt token to users

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -14,8 +14,7 @@ usersRoute.post('/', async (request, response) => {
       password,
     });
 
-    const { id, created_at, updated_at } = user;
-    return response.json({ id, name, email, created_at, updated_at });
+    return response.json(user);
   } catch (err) {
     return response.status(400).json({ message: err.message });
   }
