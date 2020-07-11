71c307fad7486a3ce1d601a62d7912a4ccabfdbc
Diogo Queiroz <diogo.qds97@gmail.com>
2020-07-11 12:06:37 -0300

    upload avatar image

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,7 +1,11 @@
 import { Router } from 'express';
+import multer from 'multer';
 import CreateUserService from '../services/CreateUserService';
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
+import uploadConfig from '../config/upload';
 
 const usersRoute = Router();
+const upload = multer(uploadConfig);
 
 usersRoute.post('/', async (request, response) => {
   try {
@@ -19,4 +23,13 @@ usersRoute.post('/', async (request, response) => {
     return response.status(400).json({ message: err.message });
   }
 });
+
+usersRoute.patch(
+  '/avatar',
+  ensureAuthenticated,
+  upload.single('avatar'),
+  async (request, response) => {
+    return response.json({ ok: true });
+  },
+);
 export default usersRoute;
