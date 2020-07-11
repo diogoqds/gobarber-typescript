55b5ec0ae4fde85c0a6958913998ea3a13e18e46
Diogo Queiroz <diogo.qds97@gmail.com>
2020-07-11 17:04:58 -0300

    saving avatar photo

---
+++ src/services/UpdateUserAvatarService.ts
@@ -0,0 +1,36 @@
+import path from 'path';
+import fs from 'fs';
+import { getRepository } from 'typeorm';
+import User from '../models/User';
+import uploadConfig from '../config/upload';
+
+interface Request {
+  user_id: string;
+  avatarFilename: string;
+}
+
+export default class UpdateUserAvatarService {
+  public async execute({ user_id, avatarFilename }: Request): Promise<User> {
+    const userRepository = getRepository(User);
+
+    const user = await userRepository.findOne(user_id);
+
+    if (!user) {
+      throw new Error('Only authenticated user can change avatar photo');
+    }
+
+    if (user.avatar) {
+      const userAvatarFilePath = path.join(uploadConfig.directory, user.avatar);
+
+      const userAvatarFileExists = await fs.promises.stat(userAvatarFilePath);
+
+      if (userAvatarFileExists) {
+        await fs.promises.unlink(userAvatarFilePath);
+      }
+    }
+
+    user.avatar = avatarFilename;
+    await userRepository.save(user);
+    return user;
+  }
+}
